0fa9d853b031395990df11e5441c9bdf9d57022b
mahajan-1497 <57616766+mahajan-1497@users.noreply.github.com>
2019-11-26 11:59:19 +0530

    Add files via upload

---
+++ operators.py
@@ -0,0 +1,83 @@
+#Assignment
+'''
+=
+
+'''
+a=10
+b=20
+a=b
+print(a)
+print(b)
+
+#Arithmatic
+'''
+    +
+    -
+    *
+    /
+    %
+    **
+    '''
+
+c = a+b
+c = a-b
+c = a*b
+c = a/b
+c = a%b
+c = a**2
+print(c)
+
+# compound assignment
+
+'''
+    +=
+    -=
+    *=
+    /=
+    %=
+    **=
+ '''
+
+c += a # c= c**a
+
+print(c)
+
+
+# Comparision 
+
+'''
+    <
+    >
+    >=
+    <=
+    ==
+    !=
+'''
+
+a=20
+c = a<b
+
+c = a!=b
+print(c)
+
+# Logical
+ 
+'''
+    and
+    or
+    not
+    p  q   and   or   not (p)
+    1  1    1     1     0
+    1  0    0     1     0
+    0  1    0     1     1 
+    0  0    0     0     1
+'''
+c = not(a==10) and (b==20)
+
+print(c)  
+
+a = True
+b = True
+
+c = a+b 
+print (c)
