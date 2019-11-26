f06d9def42adf4a2f1142bba5dc32576d295bb0a
Priyanka-stat <priyankamahajan2013@gmail.com>
2019-11-26 13:37:35 +0530

    control flow

---
+++ controlflow.py
@@ -0,0 +1,17 @@
+'''
+If condition:
+    statement1
+    statement2
+else:
+    Statement3
+    statement4
+'''
+
+a = 20 
+b = -5
+
+if a<b:
+    print('a is less than b')
+else:
+    print('a is greater than b')
+    
