797ee88968876a1b5921d8537149342dc847b494
Priyanka-stat <priyankamahajan2013@gmail.com>
2019-11-27 11:49:57 +0530

    odd even number

--- OddEven.py
+++ OddEven.py
@@ -3,17 +3,7 @@ if num % 2 ==0 :
     print('number is even')
 else:
     print('number is odd')
-
-
-if num>1:
-    for i in range(2,num):
-        if num % i==0:
-            print('number is not prime')
-            break
-    else:
-        print("The number is Prime")
-else:
-    print("The number is not Prime")       
+    
          
 
 
